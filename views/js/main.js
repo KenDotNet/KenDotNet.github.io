8013d5f25315bd637a2a2208455b327fe50c2d0e
Ken Pritchard <ken@kens-mbp.gateway.pace.com>
2015-02-28 14:47:40 -0500

    user variable for collection

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var elementList = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < elementList.length; i++) {
+      var dx = determineDx(elementList[i], size);
+      var newwidth = (elementList[i].offsetWidth + dx) + 'px';
+      elementList[i].style.width = newwidth;
     }
   }
 
